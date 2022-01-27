2c4484f4849dc2eeb161f399939777a26113e6d3
Hy <rekind133@outlook.com>
2022-01-28 00:24:51 +0800

    add CRT.hpp

---
+++ src/CRT.hpp
@@ -0,0 +1,44 @@
+#pragma once
+
+#include <vector>
+#include <functional>
+#include <cstdint>
+#include <cassert>
+
+template <typename T>
+static T extgcd(T a, T b, T& x, T& y) {
+  if (b == 0) {
+    x = 1, y = 0;
+    return a;
+  }
+  T d = extgcd(b, a % b, y, x);
+  y -= a / b * x;
+  return d;
+}
+
+/**
+ * @brief 中国剩余定理 (Chinese Reminder Theorem)
+ *        nZ_{n} = Z_{n_{1}} \times Z_{n_{2}} \times \cdots \times Z_{n_{k}}
+ *        简介: https://crypto.stanford.edu/pbc/notes/numbertheory/crt.html 
+ *        模板题: https://www.luogu.com.cn/problem/P1495
+ * 
+ * @tparam T 
+ * @param bs 
+ * @param ns 
+ * @return T 
+ */
+template<typename T>
+T CRT(const std::vector<T> &bs, const std::vector<T> &ns) {
+  assert(bs.size() == ns.size());
+  int n = bs.size();
+  T N = 1;
+  for (const auto x : ns) N *= x;
+  T ans = 0;
+  for (int i = 0; i < n; ++i) {
+    T b, y;
+    extgcd(N / ns[i], ns[i], b, y);
+    ans += bs[i] * N / ns[i] * b;
+    ans = (ans % N + N) % N;
+  }
+  return ans;
+}
