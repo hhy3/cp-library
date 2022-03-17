c8413811977f60fc88584bdd47dc7e9bae218217
Hy <rekind133@outlook.com>
2022-03-17 16:43:36 +0800

    add string_hash

--- include/hy/string.hpp
+++ include/hy/string.hpp
@@ -2,6 +2,8 @@
 
 #include <vector>
 #include <string>
+#include <string_view>
+
 
 namespace hy {
 namespace string {
@@ -9,7 +11,7 @@ namespace string {
 /**
  * @brief kmp (Knuth–Morris–Pratt) 算法
  *        
- *        简介: https://en.wikipedia.org/wiki/Knuth%E2%80%93Morris%E2%80%93Pratt_algorithm
+ *        简介: https://cp-algorithms.com/string/prefix-function.html
  *        模板题: https://www.luogu.com.cn/problem/P3375
  * 
  *        Time Complexity: O(n + m)
@@ -46,6 +48,58 @@ std::pair<std::vector<int>, std::vector<int>> kmp(const std::string &s, const st
   return {ans, pi};
 }
 
+
+/**
+ * @brief 字符串哈希
+ * 
+ *        简介: https://oi-wiki.org/string/hash/
+ *        模板题: luogu.com.cn/problem/P3370
+ * 
+ * @tparam K 
+ */
+template <uint64_t K=13331>
+struct string_hash {
+
+  int n;
+  std::vector<uint64_t> h, power;
+
+
+  /**
+   * @brief Construct a new string hash object
+   *
+   *        Time Complexity: O(n)
+   *  
+   */
+  string_hash(std::string_view s)
+    : n((int)s.size()), h(n+1), power(n+1) {
+    power[0] = 1;
+    for (int i = 1; i <= n; ++i) {
+      h[i] = h[i-1] * K + s[i-1];
+      power[i] = power[i-1] * K;
+    }  
+  }
+
+  /**
+   * @brief get hash value of substring s[l:r] 
+   *        
+   *        Time Complexity: O(1)
+   * 
+   */
+  uint64_t get(int l, int r) {
+    return h[r+1] - h[l] * power[r - l + 1];
+  }
+
+  /**
+   * @brief get hash value of the whole string s
+   *        
+   *        Time Complexity: O(1)
+   * 
+   */
+  uint64_t get() {
+    return get(0, n-1);
+  }
+
+};
 
 } // namespace string
 } // namespace hy
