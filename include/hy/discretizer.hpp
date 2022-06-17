f385d03acab7f6373aec74df53387ea79b01b2c8
Hy <rekind133@outlook.com>
2022-06-17 23:49:10 +0800

    add discretizer.hpp

---
+++ include/hy/discretizer.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <vector>
+#include <map>
+#include <algorithm>
+#include <numeric>
+
+namespace hy {
+namespace misc {
+
+template <typename T=int64_t>
+struct Discretizer {
+  std::map<T, int> mp;
+  int sz = 0;
+  template <typename Iter> Discretizer(Iter first, Iter last) {
+    for (auto it = first; it != last; ++it) mp[*it];
+    for (auto& [k, v] : mp) v = ++sz;
+  }
+  int operator [] (T x) { return mp[x]; }
+};
+
+}
+}
