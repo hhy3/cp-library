fe403bcaf095c78556e02121c64f20361e69b24c
Hy <rekind133@outlook.com>
2022-06-18 03:46:29 +0800

    some shit

--- include/hy/sparse_table.hpp
+++ include/hy/sparse_table.hpp
@@ -10,7 +10,6 @@ struct sparse_table {
   std::vector<int> log; 
   std::vector<std::vector<T>> f;
   idem_op op;
-
   sparse_table(const std::vector<T>& v, idem_op op=[](T x, T y) { return min(x, y); })
     : n((int)v.size()), log(n+1), op(op) {
     for (int i = 2; i <= n; ++i) log[i] = log[i>>1] + 1;
@@ -20,10 +19,8 @@ struct sparse_table {
       for (int j = 0; j + (1 << i) - 1 < n; ++j) 
         f[i][j] = op(f[i-1][j], f[i-1][j+(1<<(i-1))]);
   }
-
   T query(int L, int R) {
     int j = log[R-L+1];
     return op(f[j][L], f[j][R-(1<<j)+1]);
   }
-
 };
--- include/hy/tree_algos.hpp
+++ include/hy/tree_algos.hpp
@@ -9,7 +9,6 @@
 namespace hy {
 namespace tree {
 
-
 struct LCA {
   using tree_t = std::vector<std::vector<int>>;
   static constexpr int MAXM = 20;
@@ -17,30 +16,21 @@ struct LCA {
   int n, r;
   std::vector<std::array<int, MAXM>> f;
   std::vector<int> depth;
-
   LCA(tree_t& G, int n, int r): G(G), n(n), r(r), f(n + 1), depth(n + 1) { dfs(r, 0); }
-
   int get_lca(int u, int v) {
     if (depth[u] < depth[v]) std::swap(u, v);
-    for (int j = MAXM - 1; j >= 0; --j) {
-      if (depth[f[u][j]] >= depth[v]) u = f[u][j];
-    }
+    for (int j = MAXM - 1; j >= 0; --j) if (depth[f[u][j]] >= depth[v]) u = f[u][j];
     if (u == v) return u;
-    for (int j = MAXM - 1; j >= 0; --j) {
-      if (f[u][j] != f[v][j]) u = f[u][j], v = f[v][j];
-    }
+    for (int j = MAXM - 1; j >= 0; --j) if (f[u][j] != f[v][j]) u = f[u][j], v = f[v][j];
     return f[u][0];
   }
-
   void dfs(int u, int p) {
     depth[u] = depth[p] + 1;
     f[u][0] = p;
     for (int i = 1; i < MAXM; ++i) f[u][i] = f[f[u][i - 1]][i - 1];
     for (auto v : G[u]) if (v != p) dfs(v, u);
   }
-
 };
-
 
 struct heavy_light_decomposition {
   using tree_t = std::vector<std::vector<int>>;
@@ -48,7 +38,6 @@ struct heavy_light_decomposition {
   tree_t& G;
   std::vector<int> parent, depth, heavy, head, pos, dfn;
   int cur_time = 0;
-
   heavy_light_decomposition(tree_t& G, int s, int n): n(n), G(G) {
     parent.resize(n + 1);
     depth.resize(n + 1);
@@ -59,7 +48,6 @@ struct heavy_light_decomposition {
     dfs(s);
     decompose(s, s);
   }
-
   int dfs(int u) {
     int sz = 1, mx_sz = 0;
     for (auto v : G[u]) if (v != parent[u]) {
@@ -71,19 +59,15 @@ struct heavy_light_decomposition {
     }
     return sz;
   }
-
   void decompose(int u, int h) {
     head[u] = h;
     dfn[u] = ++cur_time;
     if (heavy[u] != -1) decompose(heavy[u], h);
     for (auto v : G[u]) if (v != parent[u] && v != heavy[u]) decompose(v, v);
   }
-
 };
 
-
 struct Prufer {
-
   static std::vector<int> encode(const std::vector<std::vector<int>> &G) {
     int n = G.size();
     std::vector<int> parent(n, -1), code(n-2), deg(n);
@@ -106,7 +90,6 @@ struct Prufer {
     }
     return code;
   }
-
   static std::vector<std::vector<int>> decode(const std::vector<int> &code) {
     int n = code.size() + 2;
     std::vector<int> deg(n, 1);
