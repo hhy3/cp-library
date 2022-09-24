64e083f60df639c542a2f2d4c0eec16612ab6e81
zh Wang <rekind133@outlook.com>
2022-09-24 18:24:49 +0800

    Update StaticQueue.hpp

--- hy/StaticQueue.hpp
+++ hy/StaticQueue.hpp
@@ -5,17 +5,20 @@
 namespace hy {
 namespace ds {
 
-template <typename T>
-struct StaticQueue {
-  std::vector<T> buf;
-  int head, tail;
-  StaticQueue(int cap): buf(cap), head(0), tail(0) {}
-  int size() { return tail - head; }
-  void push(const T& x) { buf[tail++] = x; }
-  T pop() { return buf[head++]; }
-  T& front() { return buf[head]; }
-  void clear() { head = tail = 0; }
-};
+namespace StaticQueue {
+// modify these two lines if needed.
+using T = int;
+constexpr int N = 1e6 + 10;
+
+T buf[N];
+int head = 0, tail = 0;
+int size() { return tail - head; }
+void push(const T& x) { buf[tail++] = x; }
+void pop() { head++; }
+T& front() { return buf[head]; }
+void clear() { head = tail = 0; }
+
+}  // namespace StaticQueue
 
 }  // namespace ds
 }  // namespace hy
