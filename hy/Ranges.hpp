6442289767d15244ed970be70a852ffa82c29000
zh Wang <rekind133@outlook.com>
2022-10-12 01:03:13 +0800

    Update Ranges.hpp

--- hy/Ranges.hpp
+++ hy/Ranges.hpp
@@ -7,18 +7,17 @@ namespace hy {
 
 /**
  * Maintain a set of ranges of form \{[l_1, r_1], [l_2, r_2], ..., [l_k,
- * r_k]\} s.t. r_i < l_{i+1}
+ * r_k]\} s.t. r_i + 1 < l_{i+1}
  */
 template <typename T = int>
 struct Ranges {
-  static constexpr T mi = std::numeric_limits<T>::min(),
-                     mx = std::numeric_limits<T>::max();
+  static constexpr T mi = std::numeric_limits<T>::min() / 2, mx = std::numeric_limits<T>::max() / 2;
   std::set<std::pair<T, T>> st;
   Ranges() { st.emplace(mi, mi), st.emplace(mx, mx); }
   void add(T L, T R) {
-    for (auto it = --st.upper_bound({R + 2, mi}); it->second >= L - 1;
-         st.erase(it--))
+    for (auto it = --st.upper_bound({R + 2, mi}); it->second >= L - 1; st.erase(it--)) {
       L = min(L, it->first), R = max(R, it->second);
+    }
     st.emplace(L, R);
   }
   void remove(T L, T R) {
