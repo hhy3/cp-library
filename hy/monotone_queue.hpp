39ab95280567da964fa57aabb12cc9f41bee97f4
zh Wang <rekind133@outlook.com>
2022-10-30 01:21:06 +0800

    Update monotone_queue.hpp

--- hy/monotone_queue.hpp
+++ hy/monotone_queue.hpp
@@ -16,16 +16,18 @@ struct MonotoneQueue {
   // Sliding window mimimum: auto pred = [] (T x, T y) { return x < y; }
   std::deque<std::pair<int, T>> q;
   int window;
+  int cnt = 0;
   explicit MonotoneQueue(int window_) : window(window_) {}
   // Loop invariant: pred(q[i].second, q[i+1].second) = true
-  int insert(int idx, const T &x) {
-    while (q.size() && q.front().first <= idx - window) {
+  int insert(const T &x) {
+    cnt++;
+    while (q.size() && q.front().first <= cnt - window) {
       q.pop_front();
     }
     while (q.size() && !pred(q.back().second, x)) {
       q.pop_back();
     }
-    q.emplace_back(idx, x);
+    q.emplace_back(cnt, x);
     return q.front().first;
   }
 };
