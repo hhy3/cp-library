73a778acac3ae31aa72ab13801e74d6644e7c4f2
zh Wang <rekind133@outlook.com>
2023-02-03 03:14:41 +0800

    Update binary gcd

--- hy/math.hpp
+++ hy/math.hpp
@@ -1,20 +1,22 @@
 #pragma once
 
-#include <vector>
-#include <functional>
-#include <cstdint>
-#include <map>
 #include <cmath>
+#include <cstdint>
+#include <functional>
+#include <map>
+#include <type_traits>
+#include <vector>
 
 namespace hy {
 
-int64_t fast_gcd(int64_t x, int64_t y) {
+template <typename T>
+std::enable_if_t<std::is_integral_v<T>, T> binary_gcd(T x, T y) {
   if (x == 0) return y;
   if (y == 0) return x;
   int X = __builtin_ctz(x), Y = __builtin_ctz(y);
   int shift = std::min(X, Y);
   y >> Y;
-  for (; x; ) {
+  for (; x;) {
     x >>= X;
     int d = y - x;
     X = __builtin_ctz(d);
@@ -24,8 +26,8 @@ int64_t fast_gcd(int64_t x, int64_t y) {
   return y << shift;
 }
 
-std::vector<int> prime_sieve(int n, std::vector<int> &sieve) {
-  sieve.resize(n+1);
+std::vector<int> prime_sieve(int n, std::vector<int>& sieve) {
+  sieve.resize(n + 1);
   std::vector<int> primes;
   for (int i = 2; i <= n; ++i) {
     if (!sieve[i]) primes.push_back(i);
@@ -48,22 +50,23 @@ int64_t lucas(int64_t m, int64_t n, int64_t p) {
   std::vector<int64_t> fac(p), ifac(p), inv(p);
   fac[0] = ifac[0] = 1;
   for (int i = 1; i < p; ++i) {
-    inv[i] = (i == 1? : (p - p / i) * inv[p % i] % p);
-    fac[i] = fac[i-1] * i % p;
-    ifac[i] = ifac[i-1] * inv[i] % p;
+    inv[i] = (i == 1 ?: (p - p / i) * inv[p % i] % p);
+    fac[i] = fac[i - 1] * i % p;
+    ifac[i] = ifac[i - 1] * inv[i] % p;
   }
-  auto comb = [&] (auto m, auto n, auto p) {
-    return m >= n? fac[m] * ifac[n] % p * ifac[m-n] % p : 0;
+  auto comb = [&](auto m, auto n, auto p) {
+    return m >= n ? fac[m] * ifac[n] % p * ifac[m - n] % p : 0;
   };
-  std::function<int64_t(int64_t, int64_t, int64_t)> lucas_ = [&] (int64_t m, int64_t n, int64_t p) {
-    return m? lucas_(m / p, n / p, p) * comb(m % p, n % p, p) % p : 1;
-  };
-  return lucas_(m, n, p); 
+  std::function<int64_t(int64_t, int64_t, int64_t)> lucas_ =
+      [&](int64_t m, int64_t n, int64_t p) {
+        return m ? lucas_(m / p, n / p, p) * comb(m % p, n % p, p) % p : 1;
+      };
+  return lucas_(m, n, p);
 }
 
-template<typename T>
-T CRT(const std::vector<T> &bs, const std::vector<T> &ns) {
-  std::function<T(T, T, T, T)> extgcd = [&] (T a, T b, T& x, T& y) {
+template <typename T>
+T CRT(const std::vector<T>& bs, const std::vector<T>& ns) {
+  std::function<T(T, T, T, T)> extgcd = [&](T a, T b, T& x, T& y) {
     if (b == 0) return x = 1, y = 0, a;
     T d = extgcd(b, a % b, y, x);
     y -= a / b * x;
@@ -91,7 +94,7 @@ std::vector<int> iterate_subsets(int state) {
 
 std::vector<int> GospersHack(int n, int k) {
   std::vector<int> ans;
-  for (int i = (1 << k) - 1; i < 1 << n; ) {
+  for (int i = (1 << k) - 1; i < 1 << n;) {
     ans.push_back(i);
     i = (((i + (i & -i)) ^ i) >> __builtin_ctz(i & -i) + 2) | (i + (i & -i));
   }
@@ -99,25 +102,26 @@ std::vector<int> GospersHack(int n, int k) {
 }
 
 std::vector<std::vector<int64_t>> comb(int n, int64_t mod) {
-  std::vector<std::vector<int64_t>> C(n+1, std::vector<int64_t>(n+1));
+  std::vector<std::vector<int64_t>> C(n + 1, std::vector<int64_t>(n + 1));
   C[1][1] = 1;
   for (int i = 2; i <= n; ++i) {
     C[i][0] = C[i][i] = 1;
-    for (int j = 1; j < i; ++j) C[i][j] = (C[i-1][j-1] + C[i-1][j]) % mod;
+    for (int j = 1; j < i; ++j) C[i][j] = (C[i - 1][j - 1] + C[i - 1][j]) % mod;
   }
   return C;
 }
 
 std::vector<int64_t> Catalan(int n, int64_t mod) {
-  std::vector<int64_t> cat(n+1);
+  std::vector<int64_t> cat(n + 1);
   cat[0] = cat[1] = 1;
-  for (int i = 2; i <= n; ++i) for (int j = 0; j < i; ++j) 
-    cat[i] = (cat[i] + cat[i-j-1] * cat[j]) % mod;
+  for (int i = 2; i <= n; ++i)
+    for (int j = 0; j < i; ++j)
+      cat[i] = (cat[i] + cat[i - j - 1] * cat[j]) % mod;
   return cat;
 }
 
 int64_t InclusionExclusion(int n) {
-  auto calc = [&] (int s) -> int64_t {
+  auto calc = [&](int s) -> int64_t {
     // TODO
     return 0;
   };
@@ -125,8 +129,7 @@ int64_t InclusionExclusion(int n) {
   for (int s = 1; s < 1 << n; ++s) {
     if (__builtin_popcount(s) % 2) {
       ans += calc(s);
-    }
-    else {
+    } else {
       ans -= calc(s);
     }
   }
@@ -158,4 +161,4 @@ int64_t BSGS(int64_t a, int64_t b, int64_t mod) {
   return -1;
 }
 
-}
+}  // namespace hy
